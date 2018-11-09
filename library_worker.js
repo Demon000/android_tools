90d04005358de52a0a2503bc9b5529e5dd139f12
Demon000 <demonsingur@gmail.com>
2018-11-09 13:54:35 +0200

    android_tools: find_blobs: split out utility functions

--- library_worker.js
+++ library_worker.js
@@ -1,63 +1,11 @@
 const workerpool = require('workerpool');
-const execute = require('./utils').execute;
-
-const isImportantLibrary = require('./ignored_libraries').isImportantLibrary;
-
-const DEBUG = false;
-
-async function getLibraryStrings(path) {
-	const STRINGS_COMMAND = `strings "${path}" | grep -F ".so"`;
-
-	const output = await execute(STRINGS_COMMAND);
-	let libraries;
-
-	if (output == '') {
-		libraries = [];
-	} else {
-		libraries = output.trim().split('\n')
-	}
-
-	return libraries.filter(isImportantLibrary);
-}
-
-async function getFileArch(path, archFn) {
-	const FILE_COMMAND = `file ${path}`;
-	const ARCH_REGEX = /ELF \d{2}-bit/;
-
-	const output = await execute(FILE_COMMAND);
-	const arch = output.match(ARCH_REGEX)[0];
-
-	return arch;
-}
-
-function toLinuxWildcardLibrary(library) {
-	return library.replace('%s', '*');
-}
-
-async function getPathsForLibrary(library, filesDirectory) {
-	const wildcard = toLinuxWildcardLibrary(library);
-	const FIND_COMMAND = `find ${filesDirectory} -name "${wildcard}"`;
-
-	const output = await execute(FIND_COMMAND);
-	let paths = output.trim().split('\n');
-
-	if (output == '') {
-		paths = [];
-		if (DEBUG) {
-			console.log(`missing: ${library}`);
-		}
-	} else {
-		paths = output.trim().split('\n')
-	}
-
-	return paths;
-}
+const utils = require('./utils');
 
 async function getPathsForLibraries(libraries, filesDirectory) {
 	let allPaths = [];
 
 	for (const library of libraries) {
-		const paths = await getPathsForLibrary(library, filesDirectory);
+		const paths = await utils.getPathsForLibrary(library, filesDirectory);
 		allPaths = allPaths.concat(paths);
 	}
 
@@ -68,7 +16,7 @@ async function filterFilesWithArch(paths, targetArch) {
 	const matchingPaths = [];
 
 	for (const path of paths) {
-		const fileArch = await getFileArch(path);
+		const fileArch = await utils.getFileArch(path);
 		if (fileArch == targetArch) {
 			matchingPaths.push(path);
 		}
@@ -78,9 +26,9 @@ async function filterFilesWithArch(paths, targetArch) {
 }
 
 async function getNeededLibraries(path, filesDirectory) {
-	const fileArch = await getFileArch(path);
+	const fileArch = await utils.getFileArch(path);
 
-	const libraries = await getLibraryStrings(path);
+	const libraries = await utils.getLibraryStrings(path);
 
 	const allPaths = await getPathsForLibraries(libraries, filesDirectory);
 	const paths = await filterFilesWithArch(allPaths, fileArch);
--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
+const DEBUG = false;
+
 const exec = require('child_process').exec;
+
+const isImportantLibrary = require('./ignored_libraries').isImportantLibrary;
 
 function execute(command, outputFn) {
 	return new Promise(function(resolve, reject) {
@@ -19,6 +23,58 @@ function execute(command, outputFn) {
 	});
 }
 
+async function getLibraryStrings(path) {
+	const STRINGS_COMMAND = `strings "${path}" | grep -F ".so"`;
+
+	const output = await execute(STRINGS_COMMAND);
+	let libraries;
+
+	if (output == '') {
+		libraries = [];
+	} else {
+		libraries = output.trim().split('\n')
+	}
+
+	return libraries.filter(isImportantLibrary);
+}
+
+function toLinuxWildcardLibrary(library) {
+	return library.replace('%s', '*');
+}
+
+async function getPathsForLibrary(library, filesDirectory) {
+	const wildcard = toLinuxWildcardLibrary(library);
+	const FIND_COMMAND = `find ${filesDirectory} -name "${wildcard}"`;
+
+	const output = await execute(FIND_COMMAND);
+	let paths = output.trim().split('\n');
+
+	if (output == '') {
+		paths = [];
+		if (DEBUG) {
+			console.log(`missing: ${library}`);
+		}
+	} else {
+		paths = output.trim().split('\n')
+	}
+
+	return paths;
+}
+
+async function getFileArch(path, archFn) {
+	const FILE_COMMAND = `file ${path}`;
+	const ARCH_REGEX = /ELF \d{2}-bit/;
+
+	const output = await execute(FILE_COMMAND);
+	const arch = output.match(ARCH_REGEX)[0];
+
+	return arch;
+}
+
 module.exports = {
 	execute,
+	getLibraryStrings,
+	toLinuxWildcardLibrary,
+	getPathsForLibrary,
+	getFileArch,
 };
