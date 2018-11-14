b547e28f9b2965f2b65d154ac9dc0bc0d6f39f34
Demon000 <demonsingur@gmail.com>
2018-11-14 15:59:53 +0200

    android_tools: print list of root blobs

--- build_blob_tree.js
+++ build_blob_tree.js
@@ -188,16 +188,16 @@ async function printBlobs(dirpath) {
 		};
 	}
 
-	const usageList = [];
+	const rootBlobs = [];
 	for (const library in usage) {
-		usageList.push([library, usage[library]]);
+		if (usage[library] != 0) {
+			continue;
+		}
+
+		rootBlobs.push(library);
 	}
 
-	usageList.sort(function(a, b) {
-		return a[1] - b[1];
-	});
-
-	console.log(JSON.stringify(usageList, null, 4));
+	console.log(JSON.stringify(rootBlobs, null, 4));
 }
 
 const dirpath = path.resolve(process.argv[2]);
