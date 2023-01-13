95d4673d8e14828cc885d5f83dfe94f42b528d5d
Cosmin Tanislav <demonsingur@gmail.com>
2023-01-13 17:56:07 +0200

    Fill references and labels using data from __symbols__, __fixups__ and __local_fixups__

--- sort_dts.py
+++ sort_dts.py
@@ -4,6 +4,35 @@ import re
 import sys
 import fdt
 
+def line_offset(tabsize, offset, string):
+    offset = ' ' * (tabsize * offset)
+    return offset + string
+
+class PropWordsWithPhandles(fdt.PropWords):
+    def __init__(self, name, *args):
+        super().__init__(name, *args)
+
+        self.phandle_names = {}
+
+    def set_phandle_name(self, i, name):
+        if i in self.phandle_names:
+            raise ValueError()
+
+        self.phandle_names[i] = name
+
+    def get_dts_value(self, i, word):
+        if i in self.phandle_names:
+            return f'&{self.phandle_names[i]}'
+        else:
+            return '0x{:X}'.format(word)
+
+    def to_dts(self, tabsize: int = 4, depth: int = 0):
+        result  = line_offset(tabsize, depth, self.name)
+        result += ' = <'
+        result += ' '.join([self.get_dts_value(i, word) for i, word in enumerate(self.data)])
+        result += ">;\n"
+        return result
+
 def for_each_node(node, fn, *args, max_recurse_level=-1, recurse_level=0, **kwargs):
     if node is None:
         return
@@ -104,6 +133,82 @@ def dt_reindex_fragments(dt):
         fragment_ctx,
         max_recurse_level=1)
 
+def replace_phandle_with_label(path, name, addr, label=None, phandle_labels_map=None):
+    index = addr // 4
+    node = dt.get_node(path)
+    ref_prop = node.get_property(name)
+
+    if type(ref_prop) == fdt.PropWords:
+        new_prop = PropWordsWithPhandles(name, *ref_prop.data)
+        node.remove_property(name)
+        node.append(new_prop)
+    elif type(ref_prop) == PropWordsWithPhandles:
+        new_prop = ref_prop
+    else:
+        raise ValueError()
+
+    if not label:
+        phandle = new_prop.data[index]
+        label = phandle_labels_map[phandle][0]
+
+    new_prop.set_phandle_name(index, label)
+
+
+def dt_fill_fixups(dt):
+    fixups_node = dt.root.get_subnode('__fixups__')
+    fixups_props = {}
+
+    for prop in fixups_node.props:
+        for value in prop.data:
+            value_parts = value.split(':')
+            node_path = value_parts[0]
+            prop_name = value_parts[1]
+            prop_data_index = int(value_parts[2])
+
+            replace_phandle_with_label(node_path, prop_name, prop_data_index,
+                prop.name)
+
+    dt.root.remove_subnode('__fixups__')
+
+def dt_fill_symbols(dt):
+    SYMBOLS = '__symbols__'
+    LOCAL_FIXUPS = '__local_fixups__'
+
+    local_fixups_node = dt.root.get_subnode(LOCAL_FIXUPS)
+    symbols_node = dt.root.get_subnode(SYMBOLS)
+    phandle_labels_map = {}
+
+    for prop in symbols_node.props:
+        assert type(prop) == fdt.PropStrings
+
+        label = prop.name
+        path = prop.value
+        node = dt.get_node(path)
+        phandle = node.get_property('phandle').value
+
+        if phandle not in phandle_labels_map:
+            phandle_labels_map[phandle] = []
+
+        phandle_labels_map[phandle].append(label)
+
+        if node.label:
+            label = f'{node.label}: {label}'
+
+        node.set_label(label)
+
+    def replace_phandles_with_label(node):
+        for prop in node.props:
+            abs_node_path = prop.path.removeprefix(f'/{LOCAL_FIXUPS}')
+
+            for value in prop.data:
+                replace_phandle_with_label(abs_node_path, prop.name, value,
+                    phandle_labels_map=phandle_labels_map)
+
+    for_each_node(local_fixups_node, replace_phandles_with_label)
+
+    dt.root.remove_subnode(LOCAL_FIXUPS)
+    dt.root.remove_subnode(SYMBOLS)
+
 def sort_props(prop):
     return str(prop)
 
@@ -137,7 +242,8 @@ if __name__ == '__main__':
     else:
         raise ValueError(f'Invalid file extension')
 
-    dt_reindex_fragments(dt)
+    dt_fill_fixups(dt)
+    dt_fill_symbols(dt)
     dt_sort_nodes(dt)
 
     with open(out_dts_file, 'w') as f:
