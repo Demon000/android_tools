e181cd37d2f95f4d2db2ed9e876121d67b368514
Cosmin Tanislav <demonsingur@gmail.com>
2023-06-27 12:40:10 +0300

    Allow overriding fastboot image name

--- flash_fastboot.sh
+++ flash_fastboot.sh
@@ -1,12 +1,28 @@
 ADB_STASTE=$(adb get-state)
 
+get_part_i() {
+	name="$1"
+	index="$2"
+	parts=(${name//:/ })
+	echo "${parts[$index]}"
+}
+
+get_real_part() {
+	get_part_i "$1" 0
+}
+
+get_img_part() {
+	get_part_i "$1" 1
+}
+
 if [ "$ADB_STASTE" = "device" ]; then
 	BOOTLOADER_PARTS="boot vendor_boot dtbo recovery"
 	CAN_REBOOT_TO_BL=1
 
 	for part in "$@"
 	do
-		echo "$BOOTLOADER_PARTS" | grep -w -q "$part"
+		real_part=$(get_real_part "$part")
+		echo "$BOOTLOADER_PARTS" | grep -w -q "$real_part"
 		if [ $? -ne 0 ]; then
 			CAN_REBOOT_TO_BL=0
 		fi
@@ -21,7 +37,12 @@ fi
 
 for part in "$@"
 do
-	fastboot flash "$part" "$part.img"
+	real_part=$(get_real_part "$part")
+	img_part=$(get_img_part "$part")
+	if [ -z "$img_part" ]; then
+		img_part="$real_part.img"
+	fi
+	fastboot flash "$real_part" "$img_part"
 done
 
 fastboot reboot
